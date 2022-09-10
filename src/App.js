512f1c07a1b286f10ae532e4c0f75cfcaa4a0394
XKliBruno <104238853+XKliBruno@users.noreply.github.com>
2022-09-10 20:34:17 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import Lixeira from './componentes/lixeira.js'
+
+import "./App.css"
+
+
+export default function App(){
+  return(
+    <div className='Container'>
+      <div className='navbar'><p></p></div>
+      <div className='dashboard'></div>
+    </div>
+  );
+}
---
+++ src/componentes/lixeira.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import lixeira from './imagens/lixeira.png'
+
+export default function Lixeira(){
+    return (
+        <div>
+            <img src={lixeira} alt = "lixeira" />
+        </div>
+    );
+}
+
