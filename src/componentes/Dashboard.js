d5d5f4e3eedf3b290bbdbb7c324d1dae33fb9578
luisedujorge <luiseduu21@gmail.com>
2022-09-14 15:41:29 -0300

    Funcionalidade e alterações

--- src/componentes/Dashboard.js
+++ src/componentes/Dashboard.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import './Dashboard.css'
 import image2 from './imagens/image2.png'
-import elipse1 from './imagens/Ellipse1.svg'
 import coracao2 from './imagens/coracao2.svg'
 import polygon from './imagens/Polygon1.svg'
 import coracaoVerde from './imagens/coracaoVerde.svg'
@@ -18,18 +17,21 @@ export default function Dashboard(){
             <div id='d3'>Red Hot Chili Peppers, Coldplay e mais</div>
             <div id='d4'>Spotify - 50 músicas</div> 
             <div id='d5'>1h 7min</div> 
-            <div id='d6'>
-                <img classname='img2' src={elipse1}/>
-                <img className='img3' src={polygon}/>
+            <div>
+                <div id='d6'>
+                    <img className='img3' src={polygon}/>
+                </div> 
                 <img className='img4' src={favorite?coracao2:coracaoVerde}
                     onClick={()=>setFavorite(!favorite)}
                 />
             </div>
+           
             <div>
                 <p className='p1'>#Título</p>
                 <p className='p2'>Álbum</p>
-                <div id='d8'></div>
+                
             </div>
+            <div id='d8'></div>
         </div>
     );
 }
