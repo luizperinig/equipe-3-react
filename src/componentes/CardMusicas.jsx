5361b24ce28b3aca2c72978968074be8395364b2
Bruno_Henrique <brunohep86@gmail.com>
2022-10-08 22:02:09 -0300

    modified:   src/componentes/CardMusicas.jsx

--- src/componentes/CardMusicas.jsx
+++ src/componentes/CardMusicas.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import './CardMusicas.jsx'
+import axios from 'axios'
+
 
 export default function CardMusicas(){
+
+    const [Artistas, setArtistas] = useState([])
+
+    useEffect(()=>{
+        axios.get('endereço da api')
+            .then(res=>{
+                const dadosArtistas=res.data
+                setArtistas(dadosArtistas)
+            })
+    })
+
 
     return(
         <div className="card">
